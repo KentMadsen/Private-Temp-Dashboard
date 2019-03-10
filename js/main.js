d3098605e8b1967490fd9854ca1d18564917d9e0
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-10 08:46:38 +0100

    a

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function auto_number()
+{
+      var i = 0;
+      var rules = document.getElementsByClassName('rule-number');
+
+      var counter = 0;
+
+      for(i = 0; i < rules.length; i++)
+      {
+            counter = counter + 1;
+
+            rules[i].innerHTML = counter;
+      }
+
+}
+
+auto_number();
