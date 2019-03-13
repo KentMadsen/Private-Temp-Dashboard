345f672663e58ee00ed38197a1b9b4603a6a6cd0
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-13 10:27:17 +0100

    a

--- axiom.php
+++ axiom.php
@@ -2,32 +2,9 @@
 <html lang="en">
   <head>
     
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136121264-2">
-    </script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-136121264-2');
-    </script>
-
-    <meta charset="UTF-8">
-    
-    <meta name="viewport" 
-          content="width=device-width, initial-scale=1.0">
-
-    <meta http-equiv="X-UA-Compatible" 
-          content="ie=edge">
-
-    <link rel="stylesheet" 
-          href="./css/style.css">
-
-     <link rel="stylesheet" 
-           href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
-           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" 
-           crossorigin="anonymous">
+    <?php 
+      include "./generic/media.html";
+    ?>
 
     <title>
       Dashboard - Homepage
