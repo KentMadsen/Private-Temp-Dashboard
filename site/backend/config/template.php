9180eacc697cf0c8a32d4cb0db50f2c3eebb1131
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-20 16:27:15 +0100

    config template made

---
+++ site/backend/config/template.php
@@ -0,0 +1,12 @@
+<?php
+
+// Main configuration
+    $GLOBALS['data_directory'] = '';
+    $GLOBALS['data_config_directory'] = $GLOBALS['data_directory'] . '\configuration';
+    $GLOBALS['data_sections_directory'] = $GLOBALS['data_directory'] . '\sections';
+    $GLOBALS['data_tmp_directory'] = $GLOBALS['data_directory'] . '\tmp';
+
+    $GLOBALS['img_directory']  = '';
+    
+    
+?>
